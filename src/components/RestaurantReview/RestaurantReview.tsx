74d13c87423e16c89cfc9f201b21ad9a64fd9c1f
YuriiBoltukov <kolomiha898@gmail.com>
2023-11-30 14:31:01 +0400

    feat review style

--- src/components/RestaurantReview/RestaurantReview.tsx
+++ src/components/RestaurantReview/RestaurantReview.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Review } from '../../Models/interfaces.ts';
+import React from "react";
+import { Review } from "../../Models/interfaces.ts";
+import style from "./review.module.scss";
 
 export interface RestaurantReviewsItemProps {
-  review: Review
+  review: Review;
 }
-export const RestaurantReview: React.FC<RestaurantReviewsItemProps> = ({ review }) =>{
+
+export const RestaurantReview: React.FC<RestaurantReviewsItemProps> = ({
+  review,
+}) => {
   return (
     <>
-      <div>
-        <p>Review: {review.text}</p>
-        <p>Name: {review.user}</p>
-        <p>Grade: {review.rating}</p>
+      <div className={style.review}>
+        <p className={style.review_text}>Review: {review.text}</p>
+        <p className={style.review_author}>Name: {review.user}</p>
+        <p className={style.review_grade}>Grade: {review.rating}</p>
       </div>
     </>
   );
-}
+};
